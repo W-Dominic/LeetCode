216d1d8eb811ba17fa386728defe07d5461403cd
WDominic <dom0_o@protonmail.com>
2022-09-18 18:33:33 -0400

    better solution

--- easy/firstbadversion.rs
+++ easy/firstbadversion.rs
@@ -4,17 +4,33 @@
 
 impl Solution {
     pub fn first_bad_version(&self, n: i32) -> i32 {
-		let mut curr: i32 = n;
+		/* naiive solution
+        let mut curr: i32 = n;
         
         while (curr != 0 && self.isBadVersion(curr)) {
             curr -= 1;   
         }
         curr+1
+        */
+        
+        //binary search, more efficient
+        let mut start: i32 = 1;
+        let mut end: i32 = n;
+        let mut mid: i32 = end/2;
+        let mut curr: i32 = mid;
+        while (start <= end) {
+            mid = start + (end-start)/2;
+            let res = self.isBadVersion(mid);
+            if res {
+                curr = mid;
+                end = mid - 1; 
+            }
+            else {
+                start = mid + 1;
+            }
+            
+        }
+        curr        
     }
     
-    // bad = 1, n = 3
-    // isbad(3) -> true, 2 != 0 -> true
-    // isbad(2) -> true, 1 != 0 -> true
-    // isbad(1) -> true, 0 != 0 -> false
-
 }
