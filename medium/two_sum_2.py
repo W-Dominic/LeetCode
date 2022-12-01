3c4b250aa9dbaaa49ae72bf6aed5678381bc8715
W-Dominic <dom0_o@protonmail.com>
2022-12-01 18:47:55 -0500

    two sum 2 python

---
+++ medium/two_sum_2.py
@@ -0,0 +1,10 @@
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        # we cant use extra memory so lets use two pointers
+        p1 = 1
+        p2 = len(numbers)
+
+        while(p1 != p2):
+            if (numbers[p1-1] + numbers[p2-1] > target): p2 -= 1
+            elif (numbers[p1-1] + numbers[p2-1] < target): p1 += 1
+            else: return [p1, p2]
